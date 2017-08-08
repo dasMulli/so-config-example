8e122e30fc1f006d1fb2d5ce9f31eeec36732871
Martin Ullrich <martin.ullrich@cssteam.at>
2017-08-08 11:24:06 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace configtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false);
+            var configuration = builder.Build();
+
+            var aSection = new SomeSection();
+            var section = configuration.GetSection("SomeSection");
+            aSection = section.Get<SomeSection>();
+            Console.WriteLine($"username: {aSection.UserName}, pwd: {aSection.Password}");
+        }
+    }
+
+    public class SomeSection
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
